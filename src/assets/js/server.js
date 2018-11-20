c46d8471356bca6ff2955713b29d1d8760b992ba
philoem <philoem@nexgate.ch>
2018-11-20 15:15:59 +0100

    modifs mentions legales + politique confidentialite

--- src/assets/js/server.js
+++ src/assets/js/server.js
@@ -1,7 +1,6 @@
 /*  Serveur pour envoyer les mails  */
 let express = require('express');
 let nodemailer = require("nodemailer");
-let app = express();
 let path = require('path');
 let bodyParser = require('body-parser');
 let propertiesReader = require('properties-reader');
@@ -9,10 +8,11 @@ let cors = require('cors');
 let port = 8080;
 
 let props = propertiesReader(__dirname + '/.properties');
+let app = express();
 
+app.use (express.static(__dirname + '/src'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use (express.static (__dirname + '/app/content-form'));
 app.use(cors());
 
 app.get('/', (req,res) => {
@@ -59,30 +59,32 @@ exports.server = function() {
     });
 }
 
+/********************************/
 //const nodemailer = require('nodemailer');
 //let propertiesReader = require('properties-reader');
 //let props = propertiesReader(__dirname + '/.properties');
 //module.exports = (form) => {
-// var transporter = nodemailer.createTransport({
-// service: 'gmail',
-// host: props.get('smtp.host'),
-// secure: props.get('smtp.secure'),
-// port: props.get('smtp.port'),
-// auth: {
-// user: props.get('smtp.user'),
-// pass: props.get('smtp.password')
-// }
-// });
-//const mailOptions = {
-//    from: form.name,
-//    to: 'philoem24@gmail.com',
-//    subject: form.suject,
-//    text: form.message
-// };
-//transporter.sendMail(mailOptions, function (err, info) {
-// if (err)
-// console.log(err)
-// else
-// console.log(info);
-// });
+//    let transporter = nodemailer.createTransport({
+//        service: 'gmail',
+//        host: props.get('smtp.host'),
+//        secure: props.get('smtp.secure'),
+//        port: props.get('smtp.port'),
+//        auth: {
+//            user: props.get('smtp.user'),
+//            pass: props.get('smtp.password')
+//        }
+//    });
+//    const mailOptions = {
+//        from: form.name,
+//        to: 'philoem24@gmail.com',
+//        subject: form.suject,
+//        text: form.message
+//    };
+//    transporter.sendMail(mailOptions, function (err, info) {
+//        if (err) {
+//            console.log(err)
+//        } else {
+//            console.log(info);
+//        }
+//    });
 //}
